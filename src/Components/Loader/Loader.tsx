bf9e861571744dc54ad40bc26bb732f46b64a7a2
Romal Gaikwad <romal@Romals-MacBook-Air.local>
2024-07-05 08:49:34 +0530

    Created input bar and integrated API with it. Infinte options loading is remaining

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import classes from "./Loader.module.scss";
 
-const Loader = () => {
+interface LoaderProps {
+  color?: string;
+  width?: string;
+  height?: string;
+}
+
+const Loader = ({
+  color = "#ffffff",
+  width = "40px",
+  height = "40px",
+}: LoaderProps) => {
   return (
     <div className={classes["loader-container"]}>
-      <span className={classes["loader"]} />
+      <span
+        className={classes["loader"]}
+        style={{
+          borderTop: `3px solid ${color}`,
+          width: width,
+          height: height,
+        }}
+      />
     </div>
   );
 };
--- src/Pages/Layout/Header/Header.tsx
+++ src/Pages/Layout/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import classes from "./Header.module.scss";
 import { ReactComponent as Logo } from "../../../assets/icons/logo.svg";
+import { ReactComponent as SearchIcon } from "../../../assets/icons/searchIcon.svg";
 import TabbedComponent from "../../../Components/TabbedComponent/TabbedComponent.tsx";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../../Redux/store.ts";
@@ -8,6 +9,10 @@ import { selectGenreListResponse } from "../../../Redux/Slices/GenreList/getGenr
 import { getGenreListAction } from "../../../Redux/Slices/GenreList/getGenreList.slice.ts";
 import { getMovieData } from "../../../Redux/Slices/MovieList/getMovieData.slice.ts";
 import { setFilterData } from "../../../Redux/Slices/filterSlice/filter.slice.ts";
+import useDebounce from "../../../Hooks/useDebounce.ts";
+import { fetchData } from "../../../Api/apiService.ts";
+import Loader from "../../../Components/Loader/Loader.tsx";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
   startLoading: () => void;
@@ -17,8 +22,13 @@ interface HeaderProps {
 const Header = ({ startLoading, stopLoading }: HeaderProps) => {
   const dispatch = useDispatch<AppDispatch>();
   const genreListData = useSelector(selectGenreListResponse);
+  const navigate = useNavigate();
   const [genreList, setGenreList] = useState([]);
   const [activeTab, setActiveTab] = useState<number[]>([0]);
+  const [inputMovie, setInputMovie] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [inputDialog, setInputDialog] = useState(false);
+  const [suggestionLoading, setSuggestionLoading] = useState(false);
 
   useEffect(() => {
     dispatch(getGenreListAction({}));
@@ -32,10 +42,97 @@ const Header = ({ startLoading, stopLoading }: HeaderProps) => {
     setGenreList(tempGenreList);
   }, [genreListData]);
 
+  const searchClickHandler = (receivedId) => () => {
+    console.log(receivedId);
+    setInputDialog(false);
+    setSuggestions([]);
+    navigate(`/MovieDetails/${receivedId}`);
+  };
+
+  const valueChangeHandler = (event) => {
+    const { value } = event.target;
+    setInputMovie(value);
+    setSuggestionLoading(true);
+
+    // Debounce the search callback
+    handleSearch(value);
+  };
+
+  const handleSearch = useDebounce((movie) => {
+    setInputDialog(true);
+    // curl -X GET "https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&query=Inception"
+
+    fetchData(
+      `https://api.themoviedb.org/3/search/movie?api_key=2dca580c2a14b55200e784d157207b4d&query=${movie}`
+    )
+      .then((data: any) => {
+        console.log(data?.results);
+        setSuggestions(data?.results);
+      })
+      .catch((err) => {})
+      .finally(() => {
+        setSuggestionLoading(false);
+      });
+
+    console.log("Searching for:", movie);
+  }, 500);
+
   return (
     <div className={classes["header-container"]}>
-      <div className={classes["header-logo-container"]}>
-        <Logo />
+      <div className={classes["header-logo-input-container"]}>
+        <div className={classes["header-logo-container"]}>
+          <Logo />
+        </div>
+        <div className={classes["header-input-container"]}>
+          <input
+            value={inputMovie}
+            onChange={valueChangeHandler}
+            placeholder="Search"
+          />
+          <div
+            className={classes["search-icon-container"]}
+            // onClick={searchClickHandler}
+          >
+            <SearchIcon />
+          </div>
+          {inputDialog && (
+            <div className={classes["suggestion-container"]}>
+              {!suggestionLoading &&
+                suggestions &&
+                suggestions?.map((el: any, index: number) => (
+                  <div
+                    key={index}
+                    className={classes["suggestion-item"]}
+                    onClick={searchClickHandler(el.id)}
+                  >
+                    <div className={classes["suggestion-item-image-container"]}>
+                      <img
+                        src={`http://image.tmdb.org/t/p/w500/${el?.poster_path}`}
+                        width={"100%"}
+                        height={"100%"}
+                      />
+                    </div>
+                    <div className={classes["suggestion-item-title"]}>
+                      <p>{el?.title}</p>
+                      <p className={classes["suggestion-item-year"]}>
+                        {el?.release_date?.split("-")?.[0]}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              {!suggestionLoading &&
+                suggestions &&
+                suggestions.length === 0 && (
+                  <div className={classes["not-found"]}>No result found</div>
+                )}
+              {suggestionLoading && (
+                <div className={classes["loader-container"]}>
+                  <Loader color="black" height="30px" width="30px" />
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
       <div className={classes["header-tabbed-container"]}>
         <TabbedComponent
